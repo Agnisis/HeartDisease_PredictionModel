df270e9262e831f139c863f032d3ce40e747e36c
Agnisis Dutta <82666025+Agnisis@users.noreply.github.com>
2023-03-13 22:01:52 +0530

    initial

--- heartPredictionApp.py
+++ heartPredictionApp.py
@@ -5,7 +5,7 @@ import sklearn
 import streamlit as st
 
 #loading the saved model
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open(r'C:\Users\agnis\Desktop\Workspace\DEV\MACHINE LEARNING PROJECTS\DISEASE DETECTION MODELS\Heart Disease Prediction\trained_model.sav','rb'))
 
 
 # creating a function for model
